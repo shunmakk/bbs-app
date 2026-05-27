d5143848f2459d961a38a28735d0c2e64636eb30
shunmakk <shunmakajioka12@gmail.com>
2026-05-27 15:19:22 +0900

    first commit

---
+++ src/main/java/com/example/bbs_app/BbsAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.bbs_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbsAppApplication.class, args);
+	}
+
+}
