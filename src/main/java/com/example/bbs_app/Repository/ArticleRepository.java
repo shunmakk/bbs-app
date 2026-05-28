0b1e5b4a9d3b65cfd78cdf4a401cd934d0c0e280
shunmakk <shunmakajioka12@gmail.com>
2026-05-28 11:57:43 +0900

    投稿内容とコメントを表示する際、１つのSQLで実行できるようにしました。

--- src/main/java/com/example/bbs_app/Repository/ArticleRepository.java
+++ src/main/java/com/example/bbs_app/Repository/ArticleRepository.java
@@ -2,15 +2,18 @@ package com.example.bbs_app.Repository;
 
 
 import com.example.bbs_app.domain.Article;
+import com.example.bbs_app.domain.Comment;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 
 /**
@@ -22,32 +25,90 @@ public class ArticleRepository {
     @Autowired
     private NamedParameterJdbcTemplate template;
 
-    /**
-     * 投稿記事オブジェクトを生成するローマッパーです.
-     */
-    private static final RowMapper<Article> ARTICLE_ROW_MAPPER = (rs, i) -> {
-        Article article = new Article();
-        article.setId(rs.getInt("id"));
-        article.setName(rs.getString("name"));
-        article.setContent(rs.getString("content"));
-        // コメントリストは、後から追加できるように空のリストを初期化してセットしておきます
-        article.setCommentList(new java.util.ArrayList<>());
-        return article;
+    //ResultSetExtractorを使用して複数行分のデータを取得します
+    private static final ResultSetExtractor<List<Article>> ARTICLE_RESULTSET = (rs) -> {
+        List<Article> articleList = new ArrayList<>();
+        List<Comment> commentList = null;
+
+        //articlesテーブルは結合した際に複数行にわたり同じデータが出力される可能性があるため、前のarticlesテーブルのIDを保持するための変数を宣言
+        int beforeIdNum = 0;
+
+        //ResultSetオブジェクトに格納された複数のデータをList<Articles>変数に格納していく
+        while (rs.next()) {
+            int nowIdNum = rs.getInt("a_id");
+
+            if (nowIdNum != beforeIdNum) {
+                Article article = new Article();
+                article.setId(nowIdNum);
+                article.setName(rs.getString("a_name"));
+                article.setContent(rs.getString("a_content"));
+                //コメントがあった際にArticleオブジェクトのcommentListに格納するため空のArrayListをセットしておく
+                commentList = new ArrayList<Comment>();
+                article.setCommentList(commentList);
+                articleList.add(article);
+            }
+            //コメントがない場合はCommentオブジェクトを作成しないようにする
+            if (rs.getInt("c_id") != 0) {
+                Comment comment = new Comment();
+                comment.setId(rs.getInt("c_id"));
+                comment.setName(rs.getString("c_name"));
+                comment.setContent(rs.getString("c_content"));
+                //commentListにcommentオブジェクトの内容をぶちこむ！
+                Objects.requireNonNull(commentList).add(comment);
+            }
+            //現在検索しているarticleテーブルのIDを前のarticleテーブルのIDを入れるbeforeIdNumに代入する
+            beforeIdNum = nowIdNum;
+        }
+        return articleList;
     };
 
     /**
-     * 全件検索.
-     *
-     * @return 投稿一覧
+     * 投稿内容とコメントを全て取得する全件検索
      */
-    public List<Article> findAll() {
+    public List<Article> findAllArticleAndComment() {
         String sql = """
-                SELECT id, name, content
-                FROM articles
-                ORDER BY id DESC;
+                SELECT
+                a.id AS a_id,
+                a.name AS a_name,
+                a.content AS a_content,
+                c.id AS c_id,
+                c.name AS c_name,
+                c.content AS c_content
+                FROM articles a
+                LEFT OUTER JOIN comments c
+                ON a.id = c.article_id
+                ORDER BY a.id DESC, c.id ASC;
                 """;
-        return template.query(sql, ARTICLE_ROW_MAPPER);
+        return template.query(sql, ARTICLE_RESULTSET);
     }
+
+
+//    /**
+//     * 投稿記事オブジェクトを生成するローマッパーです.
+//     */
+//    private static final RowMapper<Article> ARTICLE_ROW_MAPPER = (rs, i) -> {
+//        Article article = new Article();
+//        article.setId(rs.getInt("id"));
+//        article.setName(rs.getString("name"));
+//        article.setContent(rs.getString("content"));
+//        // コメントリストは、後から追加できるように空のリストを初期化してセットしておきます
+//        article.setCommentList(new java.util.ArrayList<>());
+//        return article;
+//    };
+//
+//    /**
+//     * 全件検索.
+//     *
+//     * @return 投稿一覧
+//     */
+//    public List<Article> findAll() {
+//        String sql = """
+//                SELECT id, name, content
+//                FROM articles
+//                ORDER BY id DESC;
+//                """;
+//        return template.query(sql, ARTICLE_ROW_MAPPER);
+//    }
 
     /**
      * 投稿内容をデータベースに挿入.
